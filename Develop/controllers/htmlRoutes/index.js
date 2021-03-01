62a62e9423eb1a2b2e53b8796cd7e18c585f2e19
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-03-01 16:49:21 -0500

    wrote html routes

---
+++ Develop/controllers/htmlRoutes/index.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const router = require('express').Router();
+
+//base route
+router.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+//get 'notes.html'
+router.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+module.exports = router;
---
+++ Develop/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+// const apiRoutes = require('./controllers/apiRoutes');
+const htmlRoutes = require('./controllers/htmlRoutes');
+//require PATH and FILESYSTEM modules
+const path = require('path');
+const fs = require('fs');
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+// give HTTP easy access to front end assets
+app.use(express.static('public'));
+// import data from file
+// app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+// ====================================================
+// LISTEN
+// ====================================================
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
